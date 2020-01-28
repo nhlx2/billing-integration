84003fea5cd98253c3ed458f8f06bc3b74b97a8e
Nhlanhla <nhlx2@frikwensi.com>
2020-01-28 09:37:25 +0200

    Added a simple scheduled task

--- src/main/java/com/frikwensi/billingintegration/BillingIntegrationApplication.java
+++ src/main/java/com/frikwensi/billingintegration/BillingIntegrationApplication.java
@@ -2,8 +2,10 @@ package com.frikwensi.billingintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BillingIntegrationApplication {
 
 	public static void main(String[] args) {
